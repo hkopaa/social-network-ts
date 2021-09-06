d273a109c25f7f9c85fd2d7ed6ad29e1c6af1ca5
Alexey Pokhozhay <tixiibred@gmail.com>
2021-09-06 13:03:18 +0300

    Completing WebSocket / React / Redux

--- src/pages/Chat/ChatPage.tsx
+++ src/pages/Chat/ChatPage.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { ChatMessageType } from '../../api/chat-api'
+import { ChatMessageAPIType } from '../../api/chat-api'
 import { sendMessage, startMessagesListening, stopMessagesListening } from '../../redux/chat-reducer'
 import { AppStateType } from '../../redux/redux-store'
 
@@ -14,6 +14,8 @@ const ChatPage: React.FC = () => {
 
 const Chat: React.FC = () => {
   const dispatch = useDispatch()
+  const status = useSelector((state: AppStateType) => state.chat.status)
+
   useEffect(() => {
     dispatch(startMessagesListening())
     return () => {
@@ -23,25 +25,46 @@ const Chat: React.FC = () => {
 
   return (
     <div>
-      <Messages />
-      <AddMessageForm />
+      {status === 'error' && <div>Some error occured. Please, refresh the page.</div>}
+      <>
+        <Messages />
+        <AddMessageForm />
+      </>
     </div>
   )
 }
 
 const Messages: React.FC = () => {
   const messages = useSelector((state: AppStateType) => state.chat.messages)
+  const messagesAnchorRef = useRef<HTMLDivElement>(null)
+  const [isAutoScroll, setIsAutoScroll] = useState(false)
+
+  const scrollHandler = (e: React.UIEvent<HTMLDivElement, UIEvent>) => {
+    const element = e.currentTarget
+    if (Math.abs(element.scrollHeight - element.scrollTop - element.clientHeight) < 300) {
+      !isAutoScroll && setIsAutoScroll(true)
+    } else {
+      isAutoScroll && setIsAutoScroll(false)
+    }
+  }
+
+  useEffect(() => {
+    if (isAutoScroll) {
+      messagesAnchorRef.current?.scrollIntoView({ behavior: 'smooth' })
+    }
+  }, [messages])
 
   return (
-    <div style={{ height: '400px', overflowY: 'auto' }}>
+    <div style={{ height: '400px', overflowY: 'auto' }} onScroll={scrollHandler}>
       {messages.map((m, index) => (
-        <Message key={index} message={m} />
+        <Message key={m.id} message={m} />
       ))}
+      <div ref={messagesAnchorRef}></div>
     </div>
   )
 }
 
-const Message: React.FC<{ message: ChatMessageType }> = ({ message }) => {
+const Message: React.FC<{ message: ChatMessageAPIType }> = React.memo(({ message }) => {
   return (
     <div>
       <img src={message.photo} style={{ width: '30px' }} /> <b>{message.userName}</b>
@@ -50,12 +73,13 @@ const Message: React.FC<{ message: ChatMessageType }> = ({ message }) => {
       <hr />
     </div>
   )
-}
+})
 
 const AddMessageForm: React.FC = () => {
   const [message, setMessage] = useState('')
-  const [readyStatus, setReadyStatus] = useState<'pending' | 'ready'>('pending')
   const dispatch = useDispatch()
+
+  const status = useSelector((state: AppStateType) => state.chat.status)
 
   const sendMessageHandler = () => {
     if (!message) {
@@ -70,7 +94,7 @@ const AddMessageForm: React.FC = () => {
         <textarea onChange={(e) => setMessage(e.currentTarget.value)} value={message}></textarea>
       </div>
       <div>
-        <button disabled={false} onClick={sendMessageHandler}>
+        <button disabled={status !== 'ready'} onClick={sendMessageHandler}>
           Send
         </button>
       </div>
--- src/redux/chat-reducer.ts
+++ src/redux/chat-reducer.ts
@@ -1,11 +1,14 @@
-import { chatAPI, ChatMessageType } from './../api/chat-api'
-import { authAPI } from './../api/auth-api'
+import { chatAPI, ChatMessageAPIType, StatusType } from './../api/chat-api'
 import { FormAction, stopSubmit } from 'redux-form'
 import { BaseThunkType, InferActionsTypes } from './redux-store'
 import { Dispatch } from 'redux'
+import { v1 } from 'uuid'
+
+type ChatMessageType = ChatMessageAPIType & { id: string }
 
 let initialState = {
   messages: [] as ChatMessageType[],
+  status: 'pending' as StatusType,
 }
 
 const chatReducer = (state = initialState, action: ActionsTypes): InitialStateType => {
@@ -13,7 +16,14 @@ const chatReducer = (state = initialState, action: ActionsTypes): InitialStateTy
     case 'SN/CHAT/MESSAGES_RECEIVED':
       return {
         ...state,
-        messages: [...state.messages, ...action.payload.messages],
+        messages: [...state.messages, ...action.payload.messages.map((m) => ({ ...m, id: v1() }))].filter(
+          (m, index, array) => index >= array.length - 100
+        ),
+      }
+    case 'SN/CHAT/STATUS_CHANGED':
+      return {
+        ...state,
+        status: action.payload.status,
       }
     default:
       return state
@@ -21,14 +31,19 @@ const chatReducer = (state = initialState, action: ActionsTypes): InitialStateTy
 }
 
 export const actions = {
-  messagesReceived: (messages: ChatMessageType[]) =>
+  messagesReceived: (messages: ChatMessageAPIType[]) =>
     ({
       type: 'SN/CHAT/MESSAGES_RECEIVED',
       payload: { messages },
     } as const),
+  statusChanged: (status: StatusType) =>
+    ({
+      type: 'SN/CHAT/STATUS_CHANGED',
+      payload: { status },
+    } as const),
 }
 
-let _newMessageHandler: ((messages: ChatMessageType[]) => void) | null = null
+let _newMessageHandler: ((messages: ChatMessageAPIType[]) => void) | null = null
 const newMessageHandlerCreator = (dispatch: Dispatch) => {
   if (_newMessageHandler === null) {
     _newMessageHandler = (messages) => {
@@ -38,13 +53,25 @@ const newMessageHandlerCreator = (dispatch: Dispatch) => {
   return _newMessageHandler
 }
 
+let _statusChangedHandler: ((status: StatusType) => void) | null = null
+const statusChangedHandlerCreator = (dispatch: Dispatch) => {
+  if (_statusChangedHandler === null) {
+    _statusChangedHandler = (status) => {
+      dispatch(actions.statusChanged(status))
+    }
+  }
+  return _statusChangedHandler
+}
+
 export const startMessagesListening = (): ThunkType => async (dispatch) => {
   chatAPI.start()
-  chatAPI.subscribe(newMessageHandlerCreator(dispatch))
+  chatAPI.subscribe('messages-received', newMessageHandlerCreator(dispatch))
+  chatAPI.subscribe('status-changed', statusChangedHandlerCreator(dispatch))
 }
 
 export const stopMessagesListening = (): ThunkType => async (dispatch) => {
-  chatAPI.unsubscribe(newMessageHandlerCreator(dispatch))
+  chatAPI.unsubscribe('messages-received', newMessageHandlerCreator(dispatch))
+  chatAPI.unsubscribe('status-changed', statusChangedHandlerCreator(dispatch))
   chatAPI.stop()
 }
 
